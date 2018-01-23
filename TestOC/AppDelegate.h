0c23d48488e1d764e71af58e5b6e3ae1a2e79b30
bing <bing@bingdemacbook-pro.local>
2018-01-23 04:22:21 -0800

    Initial Commit

---
+++ TestOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestOC
+//
+//  Created by bing on 2018/1/23.
+//  Copyright © 2018年 bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestOC
+//
+//  Created by bing on 2018/1/23.
+//  Copyright © 2018年 bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
